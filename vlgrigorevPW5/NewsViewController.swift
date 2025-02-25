71b6477382cfa6550b7473517bb8eabd9e76633a
vovaggri <vladimirgri0405@gmail.com>
2025-02-26 01:59:39 +0300

    Deleted Storyboard and conneted to NewsVC

--- vlgrigorevPW5/NewsViewController.swift
+++ vlgrigorevPW5/NewsViewController.swift
@@ -11,6 +11,8 @@ final class NewsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
