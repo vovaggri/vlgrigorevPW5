c1ac98863a916f9e80f876afd938c71b157cc452
vovaggri <vladimirgri0405@gmail.com>
2025-02-26 19:34:30 +0300

    Initial of tableView

--- vlgrigorevPW5/NewsViewController.swift
+++ vlgrigorevPW5/NewsViewController.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 final class NewsViewController: UIViewController {
+    
+    private let tableView: UITableView = UITableView(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.dataSource = self
     }
 
 
+}
+
+extension NewsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Cell number \(indexPath.row)"
+        cell.selectionStyle = .none
+        return cell
+    }
 }
 
